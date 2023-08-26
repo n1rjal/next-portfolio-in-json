d5a9ae1f88ff1c451ab53a14dace352f7a7687a3
n1rjal <nirjalpaudel54312@gmail.com>
2023-08-26 15:51:35 +0545

    fix: design issues

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,6 +1,6 @@
 import { useProfileStore } from "@/context/data-store";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 
 const Work = () => {
   const { data } = useProfileStore(({ host, setHost, setData, data }) => ({
@@ -9,10 +9,6 @@ const Work = () => {
     setHost,
     setData,
   }));
-
-  const [showExp, setShowExp] = useState(
-    new Array(data?.workExperiences.length).fill(false)
-  );
 
   if (!data) return <></>;
 
@@ -25,21 +21,9 @@ const Work = () => {
     });
   };
 
-  const showExpClickHandler = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    index: number
-  ) => {
-    event.preventDefault();
-    setShowExp((prev) => {
-      const newArr = [...prev];
-      newArr[index] = !newArr[index];
-      return newArr;
-    });
-  };
-
   return (
     <div className="grid grid-cols-1 gap-x-5 xl:px-10 lg:px-10 sm:gap-y-10 gradiented sectioned">
-      <div className="sm:p-0">
+      <div className="sm:p-0 order-2">
         <div className="xl:bg-white lg:bg-white xl:p-5 lg:p-5 xl:rounded-lg lg:rounded-lg">
           <h1
             className="mb-[10px] font-bold text-3xl md:text-3xl sm:text-2xl lg:text-4xl sm:p-0"
@@ -57,41 +41,37 @@ const Work = () => {
           ))}
         </div>
       </div>
-      <div className="sectioned w-[100%] sm:p-0 lg:p-10">
+      <div className="sectioned w-[100%] sm:p-0 lg:p-10 order-1 ">
         <h1 className="mb-[10px] font-bold text-3xl lg:text-4xl" id="work">
           Work
         </h1>
         <div className="my-[10px] w-[100%] p-0">
-          {data.workExperiences.map((work, index) => (
+          {data.workExperiences.map((work) => (
             <div
               className="my-[5px] w-[100%]"
               key={work.title + work.companyName}
             >
-              <div
-                className="py-5 cursor-pointer"
-                onClick={(e) => showExpClickHandler(e, index)}
-              >
-                <h2 className="text-2xl font-semibold">
+              <div className="py-5 cursor-pointer">
+                <h2 className="text-xl">
                   {work.title} - {work.companyName}
                 </h2>
                 <p className="text-sm">
                   {work.city} | {prettyPrintDate(work.startDate)} -{" "}
                   {prettyPrintDate(work.endDate)}
                 </p>
-                <ul itemType="list">
-                  {showExp[index] &&
-                    work.description.map((desc) => (
-                      <li key={desc} className="ml-[20px]">
-                        {desc}
-                      </li>
-                    ))}
+                <ul itemType="list" className="list-disc ml-[1cm]">
+                  {work.description.map((desc) => (
+                    <li key={desc} className="ml-[20px] text-md">
+                      {desc.substring(0, 1).toUpperCase() + desc.substring(1)}
+                    </li>
+                  ))}
                 </ul>
               </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="">
+      <div className="order-3">
         <div className="sectioned w-[100%] sm:p-0 lg:p-10">
           <h1 className="mb-[10px] font-bold text-3xl lg:text-4xl" id="awards">
             Awards
