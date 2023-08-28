b48f9d8e0e37f17d03d213a1f4b1ac975fffee41
n1rjal <nirjalpaudel54312@gmail.com>
2023-08-28 15:44:28 +0545

    fix: broken slugify regex fixed

--- src/app/blogs/slugify.ts
+++ src/app/blogs/slugify.ts
@@ -1,7 +1,7 @@
 import slugify from "slugify";
 
 export function customSlugify(input: string): string {
-  const symbolRegex = /[-!$%^&*()_+|~=`{}\\[\]:";'<>?,.\\/]/gi;
+  const symbolRegex = /[!$%^&*()_+|~=`{}\\[\]:";'<>?,.\\/]/gi;
   const removeRegex = /^(?![a-zA-Z0-9,-]+$)[a-zA-Z0-9-]+$/gi;
   return slugify(
     input.replaceAll(" ", "-").replaceAll(symbolRegex, "").toLowerCase(),
