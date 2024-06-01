aeda73154208560d4099cc721ab4ed52e9511754
Nirjal Paudel <nirjalpaudel54312@gmail.com>
2024-06-02 03:30:35 +0545

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ import Project from "@/components/project";
 import Skills from "@/components/skills";
 import Work from "@/components/work";
 import React from "react";
+import Head from 'next/head'
+
+
 
 export default function Home() {
   return (
-    <div>
+    <div> 
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-2089553262384227">
+      </Head>
       <Hero />
       <Work />
       <Project />
